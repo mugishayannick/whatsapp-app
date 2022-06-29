dde6e3e26bd20469c6aba699ff1d67f10586cd86
mugishayannick <yannickmugisha23@gmail.com>
2022-06-29 19:32:11 +0200

    Add navigation bar of the whatsapp template

---
+++ whatsapp-app/src/components/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import SideBar from './side/sideComponent';
+
+
+function App() {
+    return(
+        <>
+        <SideBar />
+    
+        <section  className="message-view">
+            <div>
+               
+            </div>
+        </section>
+        </>
+    );
+}
+
+export default App;
---
+++ whatsapp-app/src/components/side/sideComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './sideComponent.css';
+import LeftNavBar from '../navBar/leftNavBar'
+
+function sideBar() {
+    return (
+        <>
+        <div className="messages-list">
+
+        <LeftNavBar/>
+
+    
+        </div>
+        </>
+    );
+}
+
+export default sideBar;
